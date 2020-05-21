8af2ca652549819802e5d3e9cae0c55a2ebde4ce
shaikfaruk <shaikfaruk04@gmail.com>
2020-05-21 20:10:51 +0530

    first commit

---
+++ src/test/java/webdrivermaveen/TestLogin.java
@@ -0,0 +1,16 @@
+package webdrivermaveen;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestLogin {
+	public static void main(String args[]){
+		 WebDriverManager.chromedriver().setup();
+		 WebDriver web=  new ChromeDriver();
+		 
+		 web.get("http://www.google.com");
+	}
+
+}
