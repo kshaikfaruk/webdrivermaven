a71c15540625aed66d728530a4c17b6eeb7f52c6
shaikfaruk <shaikfaruk04@gmail.com>
2020-05-21 21:13:56 +0530

    thrid

--- src/test/java/webdrivermaveen/TestLogin.java
+++ src/test/java/webdrivermaveen/TestLogin.java
@@ -2,11 +2,13 @@ package webdrivermaveen;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestLogin {
-	public static void main(String args[]){
+	@Test
+ public void doLogin(){
 		 WebDriverManager.chromedriver().setup();
 		 WebDriver web=  new ChromeDriver();
 		 web.get("http://www.google.com");
